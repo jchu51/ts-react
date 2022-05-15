d81452fd8857b2341db57ed1e6497d14f580219b
Jonathan Chueh <jonahtanchueh1992@gmail.com>
2022-05-15 20:23:06 +1000

    feat(*) add browser list support

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,21 @@ const MinCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 let mode = "development";
-
+let target = "web";
 if (process.env.NODE_ENV === "production") {
   mode = "production";
+  target = "browserslist";
 }
 
 module.exports = {
   mode,
-
+  target,
   devtool: "source-map",
 
   module: {
     rules: [
       {
-        test: /\.s?css$/i,
+        test: /\.(s[ac]|c)css$/i,
         use: [
           MinCssExtractPlugin.loader,
           "css-loader",
